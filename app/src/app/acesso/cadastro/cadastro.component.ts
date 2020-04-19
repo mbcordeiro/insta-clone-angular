d0adf09f36d6a593871954383919b5132179360e
Matheus Cordeiro <matheusdebarroscordeiro@gmail.com>
2020-04-19 19:49:05 -0300

    Favicon

--- app/src/app/acesso/cadastro/cadastro.component.ts
+++ app/src/app/acesso/cadastro/cadastro.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-cadastro',
-  templateUrl: './cadastro.component.html',
-  styleUrls: ['./cadastro.component.css']
+  selector: "app-cadastro",
+  templateUrl: "./cadastro.component.html",
+  styleUrls: ["./cadastro.component.css"],
 })
 export class CadastroComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
