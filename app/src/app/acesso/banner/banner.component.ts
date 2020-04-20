62a19b2a3d0322e087511c761344fcc7c2e8ff1b
Matheus Cordeiro <matheusdebarroscordeiro@gmail.com>
2020-04-20 13:43:51 -0300

    Animations, tiggers, states e styles

--- app/src/app/acesso/banner/banner.component.ts
+++ app/src/app/acesso/banner/banner.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { trigger, state, style } from "@angular/animations";
 
 @Component({
-  selector: 'app-banner',
-  templateUrl: './banner.component.html',
-  styleUrls: ['./banner.component.css']
+  selector: "app-banner",
+  templateUrl: "./banner.component.html",
+  styleUrls: ["./banner.component.css"],
+  animations: [
+    trigger("banner", [
+      state(
+        "escondido",
+        style({
+          opacity: 0
+        })
+      ),
+      state(
+        "visivel",
+        style({
+          opacity: 1
+        })
+      ),
+    ]),
+  ],
 })
 export class BannerComponent implements OnInit {
+  public estado: string = "visivel";
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
