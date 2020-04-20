8858014bdd850ba5747b41373de715cfc4a87b37
Matheus Cordeiro <matheusdebarroscordeiro@gmail.com>
2020-04-20 19:02:42 -0300

    Login Autenticado

--- app/src/app/acesso/login/login.component.ts
+++ app/src/app/acesso/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, EventEmitter, Output } from "@angular/core";
-
+import { FormGroup, FormControl } from "@angular/forms";
+import { Autenticacao } from "../../autenticacao.service";
 @Component({
   selector: "app-login",
   templateUrl: "./login.component.html",
@@ -7,11 +8,24 @@ import { Component, OnInit, EventEmitter, Output } from "@angular/core";
 })
 export class LoginComponent implements OnInit {
   @Output() public exibirPainel: EventEmitter<string> = new EventEmitter();
-  constructor() {}
+
+  public formulario: FormGroup = new FormGroup({
+    email: new FormControl(null),
+    nome_completo: new FormControl(null),
+  });
+
+  constructor(public autenticacao: Autenticacao) {}
 
   ngOnInit(): void {}
 
   public exibirPainelCadastro(): void {
     this.exibirPainel.emit("cadastro");
+  }
+
+  public autenticar(): void {
+    this.autenticacao.autenticar(
+      this.formulario.value.email,
+      this.formulario.value.senha
+    );
   }
 }
--- app/src/app/autenticacao.service.ts
+++ app/src/app/autenticacao.service.ts
@@ -1,18 +1,30 @@
-import { Usuario } from './acesso/usuario.model';
+import { Usuario } from "./acesso/usuario.model";
 import * as firebase from "firebase";
 
-class Autenticacao{
-    public cadastroUsuario(usuario: Usuario): void{
-        firebase.auth().createUserWithEmailAndPassword(usuario.email, usuario.senha)
-            .then((respota: any) => {
-                delete usuario.senha;
-                firebase.database().ref(`usuario_detalhe/${btoa(usuario.email)}`)
-                    .set( usuario );
-            })
-            .catch((error: Error) => {
-                console.log(error);
-            }) 
-    }
+class Autenticacao {
+  public cadastroUsuario(usuario: Usuario): void {
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(usuario.email, usuario.senha)
+      .then((respota: any) => {
+        delete usuario.senha;
+        firebase
+          .database()
+          .ref(`usuario_detalhe/${btoa(usuario.email)}`)
+          .set(usuario);
+      })
+      .catch((error: Error) => {
+        console.log(error);
+      });
+  }
+
+  public autenticar(email: string, senha: string): void {
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, senha)
+      .then((resposta: any) => {})
+      .catch((error: Error) => console.log(error));
+  }
 }
 
-export { Autenticacao }
+export { Autenticacao };
