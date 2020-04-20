f464cab3016381523bb9d3406a1ebb73fc481aec
Matheus Cordeiro <matheusdebarroscordeiro@gmail.com>
2020-04-20 14:54:49 -0300

    Banner Animation

--- app/src/app/acesso/acesso.component.ts
+++ app/src/app/acesso/acesso.component.ts
@@ -1,11 +1,30 @@
+import { animate } from "@angular/animations";
+import { transition } from "@angular/animations";
 import { Component, OnInit } from "@angular/core";
+import { trigger, state, style } from "@angular/animations";
 
 @Component({
   selector: "app-acesso",
   templateUrl: "./acesso.component.html",
   styleUrls: ["./acesso.component.css"],
+  animations: [
+    trigger("animacao-banner", [
+      state(
+        "criacao",
+        style({
+          opacity: 1,
+        })
+      ),
+      transition("void => criado", [
+        style({ opacity: 0, trasform: 'translity(-50px, 0px)' }),
+        animate("500ms 0s ease-in-out"),
+      ]),
+    ]),
+  ],
 })
 export class AcessoComponent implements OnInit {
+  public estadoBanner: string = 'criado'
+
   constructor() {}
 
   ngOnInit(): void {}
--- app/src/app/acesso/banner/banner.component.ts
+++ app/src/app/acesso/banner/banner.component.ts
@@ -33,18 +33,32 @@ export class BannerComponent implements OnInit {
   public estado: string = "visivel";
 
   public imagens: Imagem[] = [
-    { estado: 'escondido', url: '/app/src/assets/banner-acesso/img_1.png'},
-    { estado: 'escondido', url: '/app/src/assets/banner-acesso/img_2.png'},
-    { estado: 'escondido', url: '/app/src/assets/banner-acesso/img_3.png'},
-    { estado: 'escondido', url: '/app/src/assets/banner-acesso/img_4.png'},
-    { estado: 'escondido', url: '/app/src/assets/banner-acesso/img_5.png'}
-  ]; 
+    { estado: "escondido", url: "/app/src/assets/banner-acesso/img_1.png" },
+    { estado: "escondido", url: "/app/src/assets/banner-acesso/img_2.png" },
+    { estado: "escondido", url: "/app/src/assets/banner-acesso/img_3.png" },
+    { estado: "escondido", url: "/app/src/assets/banner-acesso/img_4.png" },
+    { estado: "escondido", url: "/app/src/assets/banner-acesso/img_5.png" },
+  ];
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    setTimeout(() => this.logicaRotacao(), 2000);
+  }
 
-  public toogleEstadoAnimacao(): void {
-    this.estado = this.estado === "visivel" ? "escondido" : "visivel";
+  public logicaRotacao(): void {
+    let idx: number;
+
+    for (let i: number = 0; i <= 4; i++) {
+      if (this.imagens[i].estado === "visivel") {
+        this.imagens[i].estado = "escondido";
+        idx = i === 4 ? 0 : i + 1;
+        break;
+      }
+    }
+
+    this.imagens[idx].estado = "visivel";
+
+    setTimeout(() => this.logicaRotacao(), 2000);
   }
 }
