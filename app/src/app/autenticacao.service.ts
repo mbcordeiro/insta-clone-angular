d1aa16b5fe43759fb69e816932053a22e56a3c70
Matheus Cordeiro <matheusdebarroscordeiro@gmail.com>
2020-04-20 23:44:07 -0300

    Logout

--- app/src/app/autenticacao.service.ts
+++ app/src/app/autenticacao.service.ts
@@ -1,7 +1,7 @@
 import { Usuario } from "./acesso/usuario.model";
 import { Router } from "@angular/router";
 import * as firebase from "firebase";
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 
 @Injectable()
 class Autenticacao {
@@ -35,14 +35,32 @@ class Autenticacao {
           .currentUser.getIdToken()
           .then((idToken: string) => {
             this.token_id = idToken;
-            this.router.navigate(['/home'])
+            localStorage.setItem("idToken", idToken);
+            this.router.navigate(["/home"]);
           });
       })
       .catch((error: Error) => console.log(error));
   }
 
   public autenticado(): boolean {
+    if (
+      this.token_id === undefined &&
+      localStorage.getItem("idToken") != null
+    ) {
+      this.token_id = localStorage.getItem("idToken");
+    }
     return this.token_id !== undefined;
+  }
+
+  public sair(): void {
+    firebase
+      .auth()
+      .signOut()
+      .then(() => {
+        localStorage.removeItem("idToken");
+        this.token_id = undefined;
+        this.router.navigate(["/"]);
+      });
   }
 }
 
